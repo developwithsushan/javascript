ea37899ee9f58e1d25ee32b561cfa97aeb6899c8
Sushan Paudyal <54116821+developwithsushan@users.noreply.github.com>
2025-09-18 14:09:49 +0000

    Let, const and var ki kahani

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 98410
+let accountEmail = "sushan.paudyal@gmail.com"
+var accountPassword = "12345"
+accountCity = "Itahari"
+let accountState;
+
+// accountId = 9841099 // not allowed
+console.log(accountId)
+
+accountEmail = "mailtosushan@gmail.com"
+accountPassword = "212121"
+accountCity = "Kathmandu"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
