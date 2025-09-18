3aca38d2e999017759e51843437558156b56c309
Sushan Paudyal <54116821+developwithsushan@users.noreply.github.com>
2025-09-18 16:29:57 +0000

    Datatypes and ECMA standards

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as new version
+
+let name = "Sushan"
+let age = "32"
+let isLoggegIn = false
+
+// number => 2 to power 53
+// bigint 
+// string 
+// boolean
+// null => standalone value
+// undefined
+// symbol
+// object 
+
+console.log(typeof "Sushan Paudyal")
+console.log(typeof null);
+console.log(typeof undefined);
+
+
